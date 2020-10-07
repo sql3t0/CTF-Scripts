645265079390ed69a4afb2429bcfcb055e65a80a
sql3t0 <dj.sqleto@gmail.com>
2020-10-07 09:19:52 -0300

    Update set_cacert_burp.sh

--- set_cacert_burp.sh
+++ set_cacert_burp.sh
@@ -2,18 +2,12 @@
 
 echo "[>] Geando certificados PEM apartir do DER"
 openssl x509 -inform DER -in burp.cer -out burp.pem
-openssl x509 -inform DER -in cagece.cer -out cagece.pem
 wait
 
 burp=$(openssl x509 -inform PEM -subject_hash_old -in burp.pem |head -1).0
 wait
 echo "[>] Renomeando burp.pem para $burp"
 mv burp.pem $burp
-
-cagece=$(openssl x509 -inform PEM -subject_hash_old -in cagece.pem |head -1).0
-wait
-echo "[>] Renomeando cagece.pem para $cagece"
-mv cagece.pem $cagece
 
 echo "[>] Reiniciando o Device..."
 adb reboot
@@ -29,7 +23,7 @@ adb shell "mount -o rw,remount /"
 wait
 
 echo "[>] Enviando certificados..."
-adb push *.0 /system/etc/security/cacerts
+adb push $burp /system/etc/security/cacerts
 wait
 
 adb remount
@@ -37,6 +31,5 @@ wait
 
 echo "[>] Modificando permissoes..."
 adb shell "chmod 644 /system/etc/security/cacerts/$burp"
-adb shell "chmod 644 /system/etc/security/cacerts/$cagece"
 
 
