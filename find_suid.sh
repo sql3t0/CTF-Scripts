efaeda368d4a86be9ff10ddb50a975910ad7ed7e
sql3t0 <dj.sqleto@gmail.com>
2020-12-11 16:44:32 -0300

    Update find_suid.sh

--- find_suid.sh
+++ find_suid.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+# ssh usename@ipaddrs 'bash -s ' < find_suid.sh
+
 RED='\033[0;31m';
 GRE='\033[0;32m';
 BLU='\033[0;34m';
 LGRE='\033[1;32m';
 NC='\033[0m';
-for FILENAME in $(find / -user root -perm /4000 2>/dev/null)
-    ;do
+for FILENAME in $(find / -perm /4000 2>/dev/null)
+    do
         for DIR in $(ldd $FILENAME 2>/dev/null | grep '=>' | cut -d' ' -f 3 | xargs dirname 2>/dev/null | sort -u )
-            ;do
+            do
                 PERM=$(stat -L -c "%U" $DIR);
                 if [[ "$PERM" == "root" ]];then printf "${RED}[-]${NC} `hostname`, ${GRE}$FILENAME${NC}, ${BLU}$DIR${NC}, ${RED}$PERM ${NC}\n" ;else printf "${LGRE}[+]${NC} `hostname`, ${GRE}$FILENAME${NC}, $DIR, ${LGRE}$PERM ${NC} (Priv)\n" ;fi
-        ;done
-    ;done
+        done
+    done
