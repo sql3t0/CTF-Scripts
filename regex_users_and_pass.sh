d7004ac6bb8b4568dc6da64f722256177d363fb2
sql3t0 <dj.sqleto@gmail.com>
2020-12-16 08:01:06 -0300

    Update regex_users_and_pass.sh

--- regex_users_and_pass.sh
+++ regex_users_and_pass.sh
@@ -1,17 +1,20 @@
-#for ip in $(cat ips_ssh);do sshpass -p "P4ssw0rd!" ssh -o StrictHostKeyChecking=no username@$ip `cat regex_users_and_pass.sh`;done
-printf "\n[>] Procurando Credenciais em : `hostname` ...\n";
+#!/bin/bash
+#for ip in $(cat ips_ssh);do sshpass -p "P4ssw0rd!" ssh -o StrictHostKeyChecking=no username@$ip 'bash -s' < regex_users_and_pass.sh;done
+
+
+printf "\n[>] Procurando Credenciais em : `hostname` ...\n"
 for dir in $(find / -maxdepth 2 -type d 2>/dev/null | egrep -v "proc/|/lib|/bin|/sbin|lib*/|boot|mnt/|^/$" | egrep -v "^/\w+$")
-    ;do
-        PatternUser="user|username|user-name|usuario|login|mail|email";
-        PatternPass="pw|pass|passw|passwd|password|senha|creds|secret";
-        PatternMixx="host|ldap";
-        RegexFull="([\"\'\[\(\{\$]?)($PatternUser|$PatternPass|$PatternMixx)([\"\'\]\)\}\$]?)([ ]?)([:=])([ ]?)([\"\'\[\(\{])(.*)([\"\'\]\)\}])|([<])($PatternUser|$PatternPass|$PatternMixx)([>])(.+)([\/>])";
-        filename=$(egrep --exclude-dir={lib,bin,sbin,proc,boot,mnt} -irl "$RegexFull" $dir 2>/dev/null);
+    do
+        PatternUser="user|username|user-name|usuario|login|mail|email"
+        PatternPass="pw|pass|passw|passwd|password|senha|creds|secret"
+        PatternMixx="host|ldap"
+        RegexFull="([\"\'\[\(\{\$]?)($PatternUser|$PatternPass|$PatternMixx)([\"\'\]\)\}\$]?)([ ]?)([:=])([ ]?)([\"\'\[\(\{])(.*)([\"\'\]\)\}])|([<])($PatternUser|$PatternPass|$PatternMixx)([>])(.+)([\/>])"
+        filename=$(egrep --exclude-dir={lib,bin,sbin,proc,boot,mnt} -irl "$RegexFull" $dir 2>/dev/null)
         if [ ! -n "$filename" ]
-            ;then
+            then
                 printf "\r[+] Hostname: `hostname`  , Folder: $dir"
-            ;else
+            else
                 printf "\r[+] Hostname: `hostname`  , Folder: $dir \n";
                 egrep --color=always -aino "([\"\'\[\(\{\$]?)($PatternUser|$PatternPass|$PatternMixx)([\"\'\]\)\}\$]?)([ ]?)([:=])([ ]?)([\"\'\[\(\{])(.*)([\"\'\]\)\}])|([<])($PatternUser|$PatternPass|$PatternMixx)([>])(.+)([\/>])" $filename 2>/dev/null | cut -b 1-300 | uniq &
             fi    
-    ;done
+    done
